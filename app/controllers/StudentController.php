00d872ea32c011dab31ba976f68378af0cc9a81c
Keith Magsino <keit.lean0731@gmail.com>
2026-09-04 19:06:41 +0800

    fixed studentcontorller

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,14 +1,29 @@
 <?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+class StudentController extends Controller
 
-
-class StudentController extends BaseController
 {
-    public function index()
-    {
-        $model = new UsersModel();
 
-        $data['students'] = $model->getStudents();
+ public function index()
+ 
+ {
+ $this->call->view('student_home.php');
+ 
+ }
 
-        $this->view('student', $data);
-    }
-}
+ public function profile()
+
+ {
+
+ // Display student profile
+ $student = [
+ 'student_id' => '2026-000144',
+ 'name' => 'Keith Leanne V. Magsino',
+ 'course' => 'BS Information Technology',
+ 'year' => '3rd Year',
+ 'section' => '3-F3',
+ 'email' => 'keit.lean0731@gmail.com'
+ ];
+ $this->call->view('student_profile', $student);
+ }
+ }
